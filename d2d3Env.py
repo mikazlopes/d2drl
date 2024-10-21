c1e36df04c1aba31b1ffa9b7b68a2d66cc2e05c3
mikazlopes <miguelrtlopes@gmail.com>
2024-10-21 15:00:36 -0400

    changes to env

--- d2d3Env.py
+++ d2d3Env.py
@@ -7,9 +7,12 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import cv2
+import csv
+import os
 import logging
 import random
-import math
+from datetime import datetime
+from pathlib import Path
 from d2stream import D2GameState  # Import the class from d2stream.py
 
 # Setup logging at the start of your script
@@ -20,25 +23,82 @@ class DiabloIIGymEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     MAX_STEPS_NO_REWARD = 1000
 
-    def __init__(self, server_url, flask_port):
+    def __init__(self, server_url, flask_port, env_id='Main'):
         
         self.d2_game_state = D2GameState(flask_port)
         self.d2_game_state.run_server()
         self.cumulative_reward = 0
         self.steps_since_last_reward = 0
         self.step_counter = 0
+        self.env_id = env_id
+
+        # Initialize episode counter from the file if it exists
+        self.episode_tracker_file = 'episode_tracker_dr3_cnn.csv'
+        self.episode_counter, self.best_cumulative_reward = self.load_episode_data()
+
+        # Record break through episodes
+        self.video_writer = None
+        self.video_buffer = []
+        self.video_directory = "./videos_dr3/"  # Specify your directory here
+        os.makedirs(self.video_directory, exist_ok=True)  # Ensure the directory exists
 
         self.key_mapping = ['a', 't', 's', 'i', '1', '2', '3', '4', 'r', 'Alt', None]
         self.keyboard_action_space = len(self.key_mapping)
 
+        self.image_size = 64
+
         self.action_space = spaces.Box(low=np.array([5, 30, 0, 0], dtype=np.float32), high=np.array([795, 520, 1, self.keyboard_action_space - 1], dtype=np.float32))
-        self.observation_space = spaces.Box(low=0, high=255, shape=(64, 64, 3), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(self.image_size, self.image_size, 3), dtype=np.uint8)
         self.alt_pressed = False
         self.alt_counter = 0
 
         #self.observation = np.zeros(self.observation_space.shape, dtype=self.observation_space)
 
         self.server_url = server_url
+
+    
+    def load_episode_data(self):
+            episode_counter = 0
+            best_cumulative_reward = -float('inf')
+            if Path(self.episode_tracker_file).is_file():
+                with open(self.episode_tracker_file, mode='r') as file:
+                    csv_reader = csv.DictReader(file)
+                    for row in csv_reader:
+                        if row['env_id'] == self.env_id:
+                            episode_counter = int(row['episode_number'])
+                            best_cumulative_reward = float(row.get('best_score', -float('inf')))
+                            break
+            return episode_counter, best_cumulative_reward
+    
+    def save_episode_data(self):
+        # Load existing data
+        data = []
+        if Path(self.episode_tracker_file).is_file():
+            with open(self.episode_tracker_file, mode='r') as file:
+                data = list(csv.DictReader(file))
+
+        # Update or add the episode counter and best score for the current env_id
+        updated = False
+        for row in data:
+            if row['env_id'] == self.env_id:
+                row['episode_number'] = str(self.episode_counter)
+                row['best_score'] = str(self.best_cumulative_reward)
+                updated = True
+                break
+        if not updated:
+            data.append({
+                'env_id': self.env_id, 
+                'episode_number': str(self.episode_counter),
+                'best_score': str(self.best_cumulative_reward)
+            })
+
+        # Write updated data back to the file
+        with open(self.episode_tracker_file, mode='w', newline='') as file:
+            fieldnames = ['env_id', 'episode_number', 'best_score']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data)
+
     
     def send_request(self, url, data, max_retries=3):
         for attempt in range(max_retries):
@@ -62,7 +122,13 @@ class DiabloIIGymEnv(gym.Env):
         current_state = self.d2_game_state.get_state()
 
         mouse_x, mouse_y, mouse_click, key_index = action
-        mouse_click = 'left' if mouse_click > 0.5 else 'none'  # Simplified mouse click logic
+        # if mouse_click < 0.33:
+        #     mouse_click = 'left'
+        # elif mouse_click >= 0.33 and mouse_click < 0.66:
+        #     mouse_click = 'right'
+        # else:
+        #     mouse_click = 'none'
+        mouse_click = 'left' if mouse_click > 0.5 else 'right'  # Simplified mouse click logic
         keypress_action_key = self.key_mapping[int(key_index)]
 
 
@@ -96,32 +162,53 @@ class DiabloIIGymEnv(gym.Env):
         image = Image.open(BytesIO(response.content))
         observation_image = np.array(image)
         # Ensure the observation matches the expected shape (64, 64, 3)
-        if observation_image.shape != (64, 64, 3):
+        if observation_image.shape != (self.image_size, self.image_size, 3):
             # Resize and/or adjust observation as needed
-            observation_image = cv2.resize(observation_image, (64, 64))
+            observation_image = cv2.resize(observation_image, (self.image_size, self.image_size))
             if observation_image.ndim == 2:  # If grayscale, convert to 3 channels
                 observation_image = np.stack((observation_image,)*3, axis=-1)
 
         # Ensure observation is of type uint8
         observation_image = np.clip(observation_image, 0, 255).astype(np.uint8)
 
-        assert observation_image.shape == (64, 64, 3), "Observation shape mismatch"
+        assert observation_image.shape == (self.image_size, self.image_size, 3), "Observation shape mismatch"
+
+        # Append the original image to the video buffer for video saving
+        self.video_buffer.append(observation_image)
 
         observation = observation_image
 
-        updated_state = self.d2_game_state.get_state() 
+         # Get the updated game state after the action
+        updated_state = self.d2_game_state.get_state()
+
+        if current_state == updated_state:
+            updated_state = self.d2_game_state.get_state()
         
         done = updated_state.get('IsDead', False)
         
         reward = self.calculate_reward(new_state=updated_state, old_state=current_state)
+
         if reward <= 0:
             self.steps_since_last_reward += 1
         else:
             self.steps_since_last_reward = 0
-        self.cumulative_reward += reward
 
-        if self.steps_since_last_reward >= self.MAX_STEPS_NO_REWARD or done:
+        PENALTY = -100  # Define the penalty value
+        PENALTY_FREQUENCY = 300  # Apply the penalty every 100 steps
+
+        if self.steps_since_last_reward > 0 and self.steps_since_last_reward % PENALTY_FREQUENCY == 0:
+            reward += PENALTY  # Notice that we add because reward is typically a negative value
+
+        
+        self.cumulative_reward += reward  # Add the received reward to the cumulative reward
+
+        # Check if the reset is needed based on the custom logic
+        if self.steps_since_last_reward >= self.MAX_STEPS_NO_REWARD:
             done = True
+            reward -= 5000
+            # You can also include any additional logic here if needed before the reset
+        else:
+            done = updated_state.get('IsDead', False)
 
         self.d2_game_state.previous_quests = updated_state['CompletedQuests'].copy()
         self.d2_game_state.game_state['RemovedItems'] = []
@@ -130,7 +217,49 @@ class DiabloIIGymEnv(gym.Env):
 
         logging.info(f"Step: {self.steps_since_last_reward}, Reward: {reward}, Cumulative Reward: {self.cumulative_reward}, Done: {done}")
 
+        if done:
+            self.episode_counter += 1
+            logging.info(f'Episode {self.episode_counter} Done')
+            self.check_and_save_video()
+            self.video_buffer = []  # Reset the buffer for the next episode
+
+        reward = reward / 1e3 # Reward scaling
+
         return observation, reward, done, info
+    
+    def check_and_save_video(self):
+        if self.cumulative_reward > self.best_cumulative_reward:
+            self.best_cumulative_reward = self.cumulative_reward
+            logging.info('Saving Episode Video with new best score: {}'.format(self.best_cumulative_reward))
+            self.save_video()
+        self.save_episode_data()  # Save the updated episode counter and best score
+            
+    
+    def save_video(self):
+        current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        video_name = f"{self.video_directory}/episode_{self.episode_counter}_{int(self.cumulative_reward)}_{self.env_id}_{current_datetime}.mp4"
+        
+        # Log the length of the video buffer
+        logging.info(f'Number of frames to save: {len(self.video_buffer)}')
+
+        fourcc = cv2.VideoWriter.fourcc(*'mp4v')  # Use H.264 codec
+        out = cv2.VideoWriter(video_name, fourcc, 5.0, (self.image_size, self.image_size), True)
+
+        if not out.isOpened():
+            logging.error('VideoWriter failed to open')
+            return
+
+        try:
+            for pil_image in self.video_buffer:
+                np_frame = np.array(pil_image)  # Ensure the image is RGB
+                bgr_frame = cv2.cvtColor(np_frame, cv2.COLOR_RGB2BGR)  # Convert RGB to BGR
+                out.write(bgr_frame)  # Write the BGR frame to the video
+        except Exception as e:
+            logging.error(f'Failed to write frame: {e}')
+        finally:
+            out.release()
+            logging.info(f'Episode video saved at {video_name}')
+
 
     
     def calculate_potion_penalty_reward(self, new_state, old_state):
@@ -219,7 +348,7 @@ class DiabloIIGymEnv(gym.Env):
 
         # Reward for level up
         if new_state.get('Level', 0) > old_state.get('Level', 0):
-            reward += 100
+            reward += 1000
 
         # Penalty for death
         if new_state.get('IsDead', False):
@@ -227,11 +356,11 @@ class DiabloIIGymEnv(gym.Env):
 
         # Reward for experience gain
         if new_state.get('Experience', 0) > old_state.get('Experience', 0):
-            reward += 20
+            reward += 200
 
         # Check for new area discovery
         if new_state.get('NewAreaDiscovered', False):
-            reward += 500  # Assign the new area discovery reward
+            reward += 600  # Assign the new area discovery reward
             self.d2_game_state.game_state['NewAreaDiscovered'] = False
 
         # Quest completion rewards
@@ -286,8 +415,8 @@ class DiabloIIGymEnv(gym.Env):
         self.send_mouse_click('left')
         self.send_mouse_move(550, 325)
         self.send_mouse_click('left')
-        self.send_keypress('a')
-        self.send_keypress('b')
+        self.send_keypress('d')
+        self.send_keypress('d')
         self.send_keypress('Enter')
         time.sleep(2)
 
@@ -361,8 +490,8 @@ class DiabloIIGymEnv(gym.Env):
         self.send_mouse_click('left')
         self.send_mouse_move(550, 325)
         self.send_mouse_click('left')
-        self.send_keypress('a')
-        self.send_keypress('b')
+        self.send_keypress('n')
+        self.send_keypress('r')
         self.send_keypress('Enter')
         time.sleep(2)
 
@@ -387,8 +516,8 @@ class DiabloIIGymEnv(gym.Env):
             self.send_mouse_click('left')
             self.send_mouse_move(550, 325)
             self.send_mouse_click('left')
-            self.send_keypress('a')
-            self.send_keypress('b')
+            self.send_keypress('n')
+            self.send_keypress('n')
             self.send_keypress('Enter')
             time.sleep(4)
             # Set the flag indicating that initial reset actions have been performed
@@ -414,16 +543,16 @@ class DiabloIIGymEnv(gym.Env):
  
         observation_image = np.array(image)
         # Ensure the observation matches the expected shape (64, 64, 3)
-        if observation_image.shape != (64, 64, 3):
+        if observation_image.shape != (self.image_size, self.image_size, 3):
             # Resize and/or adjust observation as needed
-            observation_image = cv2.resize(observation_image, (64, 64))
+            observation_image = cv2.resize(observation_image, (self.image_size, self.image_size))
             if observation_image.ndim == 2:  # If grayscale, convert to 3 channels
                 observation_image = np.stack((observation_image,)*3, axis=-1)
 
         # Ensure observation is of type uint8
         observation_image = np.clip(observation_image, 0, 255).astype(np.uint8)
 
-        assert observation_image.shape == (64, 64, 3), "Observation shape mismatch"
+        assert observation_image.shape == (self.image_size, self.image_size, 3), "Observation shape mismatch"
 
         observation = observation_image
         
